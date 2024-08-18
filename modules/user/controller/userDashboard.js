d70715e6fa91f46f108e29d6b09aeaa6cbe52b57
Tharusha Theekshana <tharushatheekshana@gmail.com>
2024-08-18 09:33:46 +0530

    User dashboard API development

--- modules/user/controller/userDashboard.js
+++ modules/user/controller/userDashboard.js
@@ -1,6 +1,16 @@
-const userDashboard = (req,res) => {
+import mongoose from "mongoose";
+
+const userDashboard = async (req,res) => {
+
+    const usersModel = mongoose.model("users");
+
+    const getUser = await usersModel.findOne({
+        _id : req.user._id,
+    }).select("-password -__v -_id")
+
     res.status(200).json({
         status: "Hello from userDashboard!",
+        data: getUser
     });
 }
 
